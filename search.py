35e95397c36b9cfa5d5db54cc742e5a16c39e2d7
gramos93 <piscina.ramos@gmail.com>
2021-02-24 10:12:48 -0500

    Optimized UCS and DFS.

--- search.py
+++ search.py
@@ -86,53 +86,42 @@ def depthFirstSearch(problem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
+    # Parents tracking list
+    explored = list()
 
-    solution = list()
+    # Frontier keeps track of the branches of the graph
+    # We use a stack LIFO for the DFS algorithm (p.85)
+    frontier = util.Stack()
     
-    # Set of unique explored nodes.
-    explored = set()
+    node = (problem.getStartState(), "")
+    branch = list([node])
     
-    def recursiveDFS(child_state):
-        """
-        child_state -> [(x,y), "action", cost]
-        """
+    while not problem.isGoalState(node[0]):
+        if node[0] not in explored:
+            for child_state in problem.getSuccessors(node[0]):
+                new_branch = list(branch)
+                new_branch.append(child_state)
+                frontier.push(new_branch)
 
-        if problem.isGoalState(child_state[0]):
-            # If we found the goal we add the action to the solution list
-            solution.append(child_state[1])
-            return True
+            explored.append(node[0])
+        if frontier.isEmpty():
+            # No solution found
+            return []
 
-        elif child_state[0] in explored:
-            # We don't explore already explored nodes
-            return False
+        branch = frontier.pop()
+        node = branch[-1]
 
-        else:
-            successor_nodes = problem.getSuccessors(child_state[0])
-            # HINT: reverse the  getSuccessors list for better results
-            successor_nodes.reverse()
-            explored.add(child_state[0])
-
-            for c_state in successor_nodes:
-                if recursiveDFS(c_state):
-                    if child_state[0] != problem.getStartState():
-                        # The fist node does not have an action.
-                        # So it is excluded from this operation
-                        solution.append(child_state[1])
-
-                    return True
-    
-    recursiveDFS([problem.getStartState()])
-    # Since the solution was created from Goal to StartState,
-    # we need to reverse the order of actions.
-    solution.reverse()
-    return solution
+    solution = [action[1] for action in branch]
+    return solution[1:]
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
 
     # Parents tracking dictionary
     explored = list()
-    # Frontier keeps track of the branches branches of the graph
+
+    # Frontier keeps track of the branches of the graph
+    # We use a stack FIFO for the DFS algorithm (p.81)
     frontier = util.Queue()
     node = (problem.getStartState(), "")
     branch = list([node])
@@ -157,52 +146,35 @@ def breadthFirstSearch(problem):
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    solution = list()
-    # Parents tracking dictionary
-    # Works like and explored set.
-    parents = {}
+
+    # Parents tracking list
+    explored = list()
+    
+    # Frontier keeps track of the branches of the graph 
+    # We use a priority queue for the UCS algorithm (p.84)
     frontier = util.PriorityQueue()
+    node = (problem.getStartState(), "", 0.)
+    branch = list([[node], 0.])
+    
+    while not problem.isGoalState(node[0]):
+        if node[0] not in explored:
+            for child_state in problem.getSuccessors(node[0]):
+                new_branch = list(branch[0])
+                new_branch.append(child_state)
+                frontier.push([new_branch, branch[-1] + child_state[-1]], 
+                               branch[-1] + child_state[-1])
 
-    if problem.isGoalState(problem.getStartState()):
-        return solution
-    else:
-        frontier.push([problem.getStartState(), 0.], 0.)
-        # Log start of path so it won't be expanded twice.
-        parents[problem.getStartState()] = [0]
+            explored.append(node[0])
 
-    while not frontier.isEmpty():
+        if frontier.isEmpty():
+            # No solution found
+            return []
+       
+        branch = frontier.pop()
+        node = branch[0][-1]
 
-        node = frontier.pop()
-
-        if problem.isGoalState(node[0]):
-            while node[0] != problem.getStartState():
-                # Backtrack parents until start point
-                solution.append(parents[node[0]][1])
-                # node = it's parent position
-                node = parents[node[0]]
-
-            # Since the solution was created from Goal to StartState,
-            # we need to reverse the order of actions.
-            solution.reverse()
-            return solution
-
-        for child_state in problem.getSuccessors(node[0]):
-            if child_state[0] not in parents.keys():
-                # Log child: parent -> action -> cost
-                parents[child_state[0]] = [node[0], child_state[1],
-                        child_state[-1]]
-                # Push to frontier with update COST OF PATH
-                frontier.push((child_state[0], child_state[1],
-                    child_state[-1] + node[-1]), child_state[-1] + node[-1])
-            
-            elif parents.get(child_state[0])[-1] > node[-1] + child_state[-1]:
-                # Update cost in parents' tracking dictionary is new parent's
-                # cost is lower.
-                parents[child_state[0]] = [node[0], child_state[1],
-                                          child_state[-1]]
-                # Update to frontier with update COST OF PATH
-                frontier.update((child_state[0], child_state[1],
-                    child_state[-1] + node[-1]), child_state[-1] + node[-1]) 
+    solution = [action[1] for action in branch[0]]
+    return solution[1:]
 
 def nullHeuristic(state, problem=None):
     """
@@ -213,10 +185,12 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-# Parents tracking dictionary
+    
+    # Parents tracking list
     explored = list()
-    # Frontier keeps track not only of the nodes but all expanded 
-    # branches of the graph
+
+    # Frontier keeps track of the branches of the graph
+    # We use a priority queue with h(n)+c(n) as sorting key.
     frontier = util.PriorityQueue()
     node = (problem.getStartState(), "", 0.)
     branch = list([[node], 0.])
